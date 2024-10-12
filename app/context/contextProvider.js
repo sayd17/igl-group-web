692f57ebee453dec39d0fdb088b116f99ad58eda
Abu Sayeed <sayeed1737@gmail.com>
2024-10-12 18:08:45 +0600

    removed localstorage

--- app/context/contextProvider.js
+++ app/context/contextProvider.js
@@ -16,25 +16,26 @@ const StateContext = createContext({
 
 export const ContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
-  const [token, _setToken] = useState(cookies.get("user"));
+  const [token, _setToken] = useState(null);
   // const [token, _setToken] = useState(12323);
   const [currentSister, setCurrentSister] = useState(1);
   const [notification, _setNotification] = useState("");
 
+  const cookies = cookies();
   // Set a cookie
   // Cookies.set('user', 'JohnDoe', { expires: 7 });
 
   // // Get a cookie
   // const user = Cookies.get('user');
 
-  const setToken = (token) => {
-    _setToken(token);
-    if (token) {
+  const setToken = () => {
+    _setToken(cookies.get("token"));
+    if (cookies.has("token")) {
       // localStorage.setItem("ACCESS_TOKEN", token);
-      cookies.set("user", "Super Admin", { expires: 7 });
+      cookies.set("token", "token123");
     } else {
       // localStorage.removeItem("ACCESS_TOKEN");
-      cookies.remove("user");
+      cookies.delete("token");
     }
   };
 
